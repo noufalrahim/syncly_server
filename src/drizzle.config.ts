4e0525cf0f6c2de6461c1bfd12c886ce1ecbe344
noufalrahim <noufalrahim6784@gmail.com>
2025-06-07 00:49:18 +0530

    feat: added drizzle configs

--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -1,18 +1,25 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./src/db/schemas/*.ts",
+    out: "./drizzle",
+    dbCredentials: {
+        host: process.env.DB_HOST || 'localhost',
+        port: Number(process.env.DB_PORT) || 5432,
+        database: process.env.DB_NAME || '',
+        user: process.env.DB_USER || '',
+        password: process.env.DB_PASSWORD || '',
+        ssl: true
+    },
+});
+
 // import { defineConfig } from 'drizzle-kit';
 
 // export default defineConfig({
 //     dialect: "postgresql",
-//     // schema: "./src/db/schemas/*.ts",
-//         schema: "./db/schema.ts",
+//     schema: "./db/schema.ts",
 //     out: "./drizzle",
-//     // dbCredentials: {
-//     //     host: process.env.DB_HOST || 'localhost',
-//     //     port: Number(process.env.DB_PORT) || 5432,
-//     //     database: process.env.DB_NAME || '',
-//     //     user: process.env.DB_USER || '',
-//     //     password: process.env.DB_PASSWORD || '',
-//     //     ssl: true
-//     // },
 //     dbCredentials: {
 //         host: "localhost",
 //         port: 5432,
@@ -22,19 +29,3 @@
 //         ssl: false,
 //     },
 // });
-
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-    dialect: "postgresql",
-    schema: "./db/schema.ts",
-    out: "./drizzle",
-    dbCredentials: {
-        host: "localhost",
-        port: 5432,
-        database: "synclydb",
-        user: "postgres",
-        password: "noufalrahim",
-        ssl: false,
-    },
-});
