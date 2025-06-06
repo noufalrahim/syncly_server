b68d7fb14d96474c3612c833ea2915fd0c7fb576
noufalrahim <noufalrahim6784@gmail.com>
2025-06-07 01:03:37 +0530

    refactor: modified userOrganisation schema

--- src/db/schemas/userOrganisation.ts
+++ src/db/schemas/userOrganisation.ts
@@ -1,14 +1,14 @@
-import { pgTable, uuid, timestamp } from "drizzle-orm/pg-core";
-import { organisation } from "./organisation";
-import { user } from "./user";
+// import { pgTable, uuid, timestamp } from "drizzle-orm/pg-core";
+// import { organisation } from "./organisation";
+// import { user } from "./user";
 
-export const userOrganisation = pgTable("user_organisation", {
-    id: uuid("id").primaryKey().defaultRandom(),
-    userId: uuid("user_id").references(() => user.id, { onDelete: 'cascade' }),
-    organisationId: uuid("organisation_id").references(() => organisation.id, { onDelete: 'cascade' }),
-    createdAt: timestamp("created_at").defaultNow(),
-    updatedAt: timestamp("updated_at").defaultNow(),
-});
+// export const userOrganisation = pgTable("user_organisation", {
+//     id: uuid("id").primaryKey().defaultRandom(),
+//     userId: uuid("user_id").references(() => user.id, { onDelete: 'cascade' }),
+//     organisationId: uuid("organisation_id").references(() => organisation.id, { onDelete: 'cascade' }),
+//     createdAt: timestamp("created_at").defaultNow(),
+//     updatedAt: timestamp("updated_at").defaultNow(),
+// });
 
-export type UserOrganisation = typeof userOrganisation.$inferSelect;
-export type NewUserOrganisation = typeof userOrganisation.$inferInsert;
+// export type UserOrganisation = typeof userOrganisation.$inferSelect;
+// export type NewUserOrganisation = typeof userOrganisation.$inferInsert;
